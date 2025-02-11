e4da0f19893ca00d09697748001f1b2cdaf34e04
divyadsharma <dvsharma129@gmail.com>
2025-02-11 18:33:23 +0530

    Add test case for raising an error when string contains negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
+
+    it "throws an exception for negative numbers" do
+      calculator = StringCalculator.new
+      expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+    end
   end
 end
